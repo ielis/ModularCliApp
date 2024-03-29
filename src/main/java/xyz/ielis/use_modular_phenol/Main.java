d1ccaa83221b01a891adaf39b4db3fcc49572491
Daniel Danis <daniel.gordon.danis@protonmail.com>
2024-03-29 10:51:17 -0400

    Simplify the CLI.

--- src/main/java/xyz/ielis/use_modular_phenol/Main.java
+++ src/main/java/xyz/ielis/use_modular_phenol/Main.java
@@ -7,18 +7,11 @@ import picocli.CommandLine;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
 
-import static picocli.CommandLine.Help.Ansi.Style.*;
-
 @CommandLine.Command(name = "test-phenol",
         header = "Test modular phenol\n",
         mixinStandardHelpOptions = true,
-        usageHelpWidth = 120,
-        version = Main.VERSION,
-        footer = Main.FOOTER)
+        usageHelpWidth = 120)
 public class Main implements Callable<Integer> {
-
-    public static final String VERSION = "0.1.0-SNAPSHOT";
-    public static final String FOOTER = "That's all folks";
 
     @CommandLine.Parameters(
             arity = "1",
@@ -26,27 +19,19 @@ public class Main implements Callable<Integer> {
     )
     public Path path = null;
 
-    private static final CommandLine.Help.ColorScheme COLOR_SCHEME = new CommandLine.Help.ColorScheme.Builder()
-            .commands(bold, fg_blue, underline)
-            .options(fg_yellow)
-            .parameters(fg_yellow)
-            .optionParams(italic)
-            .build();
-
     public static void main(String[] args) {
         if (args.length == 0) {
             // if the user doesn't pass any command or option, add -h to show help
             args = new String[]{"-h"};
         }
 
-        CommandLine cline = new CommandLine(new Main())
-                .setColorScheme(COLOR_SCHEME);
+        CommandLine cline = new CommandLine(new Main());
         cline.setToggleBooleanFlags(false);
         System.exit(cline.execute(args));
     }
 
     @Override
-    public Integer call() throws Exception {
+    public Integer call() {
         // work done in subcommands
         System.err.printf("Loading from %s%n", path);
         MinimalOntology hpo = MinimalOntologyLoader.loadOntology(path.toFile());
